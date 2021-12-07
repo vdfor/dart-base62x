f9e08b9093acf348a473e7bc5a0d71d29a4b1936
vdfor <jiakun.li@qq.com>
2021-12-07 21:14:09 +0800

    fix: fix example error

--- example/example.dart
+++ example/example.dart
@@ -2,7 +2,7 @@ import 'package:base62x/base62x.dart';
 
 void main() {
   final encodeStr = Base62x.encode('hello,world'); 
-  final decodeStr = Base62x.encode('helloxdworldx');
+  final decodeStr = Base62x.decode('helloxdworldx');
 
   print('encode result ==> $encodeStr');
   print('decode result ==> $decodeStr');
